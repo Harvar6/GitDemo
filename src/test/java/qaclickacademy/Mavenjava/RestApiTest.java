cd6eaf20d2cfeddd0f4cfd11717b3018b32e7f84
Harvar6 <harshavardhan.r1@gmail.com>
2022-01-26 08:57:31 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+	
+	@Test
+	public void deleteOrkut() {
+		System.out.println("deleteOrkut");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation() {
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementUi() {
+		System.out.println("elementUI");
+	}
+}
